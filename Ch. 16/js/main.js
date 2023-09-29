2a09edf0c1caaf1463828f9fc990126a607039cf
RupertJonesSA <145080727+RupertJonesSA@users.noreply.github.com>
2023-09-28 20:11:09 -0400

    Refactored RPS Game

---
+++ Ch. 16/js/main.js
@@ -0,0 +1,44 @@
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame){
+    while(playGame){
+        const playerChoice = prompt("Please enter rock, paper, or scissors.");
+        if (playerChoice || playerChoice === ""){
+            const playerOne = playerChoice.trim().toLowerCase();
+            if (
+                playerOne === "rock" ||
+                playerOne === "paper" || 
+                playerOne === "scissors"
+            ){
+                const computerChoice = Math.floor(Math.random() * 3);
+                const rpsArray = ["rock", "paper", "scissors"];
+                const computer = rpsArray[computerChoice];
+                const result =
+                playerOne === computer
+                    ? "Tie game!"  
+                    : playerOne === "rock" && computer === "paper"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}
+                    \nComputer wins!`
+                    : playerOne === "paper" && computer === "scissors"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}
+                    \nComputer wins!`
+                    : playerOne === "scissors" && computer === "rock"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}
+                    \nComputer wins!`
+                    : `playerOne: ${playerOne}\nComputer: ${computer}
+                    \nplayerOne wins!`;
+                alert(result);
+                playGame = confirm("Play again?");
+                if (!playGame) alert("Ok, thanks for playing.")
+                continue;
+            }else{
+                alert("You didn't enter rock, paper, or scissors.");
+                continue;
+            }
+        }else{
+            alert("I guess you changed your mind. Maybe next time.");
+            break;
+        }
+    }
+}else{
+    alert("Ok, maybe next time.");
+}
